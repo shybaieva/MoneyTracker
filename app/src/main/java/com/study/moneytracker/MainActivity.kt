ff5bc38f34b3f66c459e9da6cd1c2dcb675adb5a
tamara13 <tamara13@black-box.zone>
2021-02-18 18:50:24 +0200

    init

--- app/src/main/java/com/study/moneytracker/MainActivity.kt
+++ app/src/main/java/com/study/moneytracker/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.study.moneytracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.study.moneytracker.ui.login.HomeActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mail : String
+    private lateinit var password : String
+    private lateinit var fAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fAuth = FirebaseAuth.getInstance()
+
+        loginBtn.setOnClickListener {
+            mail = mailET.text.toString()
+            password = passordET.text.toString()
+
+            if(mailET.text.isEmpty()){
+
+            }
+        }
     }
+
 }
